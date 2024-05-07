5264849abdf91273e5ecb0807e39359cb85df9be
Sara <saralefay2010@gmail.com>
2024-05-06 20:49:37 -0400

    Variables

---
+++ SampleFolder/scope_lab.js
@@ -0,0 +1,21 @@
+// Global scope
+var globalVar = "I'm a global variable";
+let globalLet = "I'm also global, but scoped with let";
+const globalConst = "I'm a global constant";
+
+
+function show(){
+// Block scope
+var blockVar = "I'm a block-scoped var";
+let blockLet = "I'm a block-scoped let";
+const blockConst = "I'm a block-scoped const";
+}
+show();
+
+console.log(functionVar);
+console.log(functionLet);
+console.log(functionConst);
+
+//console.log(blockVar);
+//console.log(blockLet);
+
